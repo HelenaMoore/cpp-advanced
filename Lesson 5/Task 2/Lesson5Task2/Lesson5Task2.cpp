8db6ffc66a45e477d38e3c031c28c969c1fa1525
Elena An <moore.helena@yandex.ru>
2023-01-18 15:14:51 +0300

    code corrections

--- Lesson 5/Task 2/Lesson5Task2/Lesson5Task2.cpp
+++ Lesson 5/Task 2/Lesson5Task2/Lesson5Task2.cpp
@@ -8,7 +8,8 @@ private:
     int rows = 0;
     int cols = 0;
 
-public:
+public:  
+    
     Table(int rows_number, int cols_number) : rows(rows_number), cols(cols_number)
     {
         table = new T * [rows];
@@ -18,9 +19,64 @@ public:
         }
     }
 
-    Table(const Table&) {};
-
+    Table(const Table&) = delete; //компилятор выдаёт ошибку C2280 при создании экземпляра, строка 101: auto test = Table<int>(2, 3);
     Table& operator=(const Table&) = delete;
+
+    /*
+    А с конструктором копирования и оператором присваивания работает:
+    _________________________________________________________________
+
+    Table(const Table& copy)
+    {
+        rows = copy.rows;
+        cols = copy.cols;
+
+        table = new T * [rows];
+        for (int i = 0; i < rows; ++i)
+        {
+            table[i] = new T[cols];
+        }
+
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                table[i][j] = copy.table[i][j];
+            }
+        }
+    };
+    
+    Table& operator=(const Table& other)
+    {
+        if (&other != this)
+        {
+            for (int i = 0; i < rows; ++i)
+            {
+                delete[] table[i];
+            }
+            delete[] table;
+
+            rows = other.rows;
+            cols = other.cols;
+
+            table = new T * [rows];
+            for (int i = 0; i < rows; ++i)
+            {
+                table[i] = new T[cols];
+            }
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < cols; ++j)
+                {
+                    table[i][j] = other.table[i][j];
+                }
+            }
+        }
+        return *this;
+    };
+    __________________________________________________________________
+    */
 
     const T* operator[](int index) const { return table[index]; }
     T* operator[](int index) { return table[index]; }
