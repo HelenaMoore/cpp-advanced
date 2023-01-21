dc2ff5691ed4db13e128e288a216f31b31cca87b
Elena An <moore.helena@yandex.ru>
2023-01-21 13:12:00 +0300

    add lesson 7 homeworks

---
+++ Lesson 7/Task 1/Lesson7Task1/Lesson7Task1.cpp
@@ -0,0 +1,36 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void eraseDuplicates(std::vector<int>& v)
+{
+    std::sort(v.begin(), v.end());
+    auto it = std::unique(v.begin(), v.end());
+    v.erase(it, v.end());
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<int> v {1, 1, 2, 5, 6, 1, 2, 4};
+
+    std::cout << "[IN]: ";
+    std::vector<int>::const_iterator i = v.cbegin();
+    while (i != v.cend())
+    {
+        std::cout << *i << " ";
+        ++i;
+    }
+    std::cout << std::endl;
+
+    eraseDuplicates(v);
+    
+    std::cout << "[OUT]: ";
+    i = v.cbegin();
+    while (i != v.cend())
+    {
+        std::cout << *i << " ";
+        ++i;
+    }
+
+    return 0;
+}
---
+++ Lesson 7/Task 2/Lesson7Task2/Lesson7Task2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <list>
+#include <set>
+#include <vector>
+
+template <class T>
+void printContainer(T& a)
+{
+    for (auto const& element : a)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    printContainer(test_set); // выведет four one three two, потому что в данном типе сортировка реализуется по принципу красно-чёрного дерева
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    printContainer(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    printContainer(test_vector);
+
+    return 0;
+}
